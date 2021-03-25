d9a185dd32b80b3edc5057394bb2947aebee8c66
Stephen Haberman <shaberman@homebound.com>
2021-03-25 08:38:35 -0500

    chore: Add rtl-utils.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,6 @@ import { configure } from "mobx";
 
 // formState doesn't use actions
 configure({ enforceActions: "never" });
+
+beforeAll(() => jest.useFakeTimers("modern"));
+afterAll(() => jest.useRealTimers());
