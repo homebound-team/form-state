3d124eb73ad8ebf6d22090badf9f31e7792a2076
Stephen Haberman <shaberman@homebound.com>
2021-12-09 15:11:07 -0600

    fix: Make ListField.value observable.
    Previously b/c the identity of the list did not change, mobx caching
    would kick-in and stop the notification.
    
    We already had this `computed false` override for the core
    formState.value, but weren't using it for child values.

--- src/formState.ts
+++ src/formState.ts
@@ -940,7 +940,10 @@ function newListFieldState<T, K extends keyof T, U>(
     },
   };
 
-  return list as any;
+  return makeAutoObservable(list, {
+    // See other makeAutoObservable comment
+    value: computed({ equals: () => false }),
+  }) as any;
 }
 
 function isNotUndefined<T>(value: T | undefined): value is T {
