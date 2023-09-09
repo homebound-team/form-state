68ee41556cf2cff3931f69b49b351fb5bec5a27a
Stephen Haberman <shaberman@homebound.com>
2023-09-08 20:37:43 -0500

    Export it.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { ObjectConfig } from "src/config";
 export { ListFieldState } from "src/fields/listField";
-export { createObjectState, ObjectState } from "src/fields/objectField";
-export { FieldState } from "src/fields/valueField";
-export { required, Rule } from "src/rules";
+export { ObjectState, createObjectState } from "src/fields/objectField";
+export { FieldState, ValueAdapter } from "src/fields/valueField";
+export { Rule, required } from "src/rules";
 export { useFormState } from "src/useFormState";
 export { useFormStates } from "src/useFormStates";
