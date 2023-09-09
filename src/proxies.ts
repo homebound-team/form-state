4c8af6bfdf85f254e70123da7ace43910a05a8ec
Stephen Haberman <shaberman@homebound.com>
2023-09-08 20:35:54 -0500

    Code golf.

--- src/proxies.ts
+++ src/proxies.ts
@@ -1,19 +1,18 @@
+/**
+ * Creates a new combined object with keys in `overrides` taking precedence, and then
+ * any other keys falling back to `delegate`.
+ */
 export function newDelegateProxy<T extends object, O extends object>(delegate: T, overrides: O): Omit<T, keyof O> & O {
+  function pickTarget(key: keyof any) {
+    return Reflect.has(overrides, key) ? overrides : delegate;
+  }
   return new Proxy(delegate, {
-    get(object, property) {
-      if (Reflect.has(overrides, property)) {
-        return Reflect.get(overrides, property);
-      } else {
-        return Reflect.get(delegate, property);
-      }
+    get(object, key) {
+      return Reflect.get(pickTarget(key), key);
     },
 
-    set(object, property, value) {
-      if (Reflect.has(overrides, property)) {
-        return Reflect.set(overrides, property, value);
-      } else {
-        return Reflect.set(delegate, property, value);
-      }
+    set(object, key, value) {
+      return Reflect.set(pickTarget(key), key, value);
     },
   }) as any;
 }
