710b5c422f40852e32c3422b8440eae17842d8c2
Stephen Haberman <shaberman@homebound.com>
2023-09-09 14:45:59 -0500

    chore: Bump storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,19 @@ module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   typescript: { check: false },
+
   webpackFinal: async (config) => {
     // Make our `src/...` imports work
     config.resolve.modules.push(__dirname, "./");
     return config;
   },
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  docs: {
+    autodocs: false
+  }
 };
