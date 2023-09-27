aa2e5c4a30ba41289c5b8cf330e71c3e418b50f8
Stephen Haberman <shaberman@homebound.com>
2023-09-27 15:25:46 -0500

    fix: Export fragment consts/types.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export { ObjectConfig } from "src/config";
 export { f } from "src/configBuilders";
+export { FragmentField } from "src/fields/fragmentField";
 export { ListFieldState } from "src/fields/listField";
-export { ObjectState, createObjectState } from "src/fields/objectField";
+export { Fragment, ObjectState, createObjectState, fragment } from "src/fields/objectField";
 export { FieldState, ValueAdapter } from "src/fields/valueField";
 export { Rule, required } from "src/rules";
 export { useFormState } from "src/useFormState";
