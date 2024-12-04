af3edb2a03de171dc0de3d2c39af8e824ae15742
Stephen Haberman <shaberman@homebound.com>
2024-12-04 09:08:06 -0600

    fix: Fix incorrect fragment export. (#117)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { f } from "src/configBuilders";
 export { type FragmentField } from "src/fields/fragmentField";
 export { type ListFieldState } from "src/fields/listField";
-export { createObjectState, type Fragment, type ObjectState, type fragment } from "src/fields/objectField";
+export { createObjectState, type Fragment, type ObjectState, fragment } from "src/fields/objectField";
 export { type FieldState, type ValueAdapter } from "src/fields/valueField";
 export { required, type Rule } from "src/rules";
 export { useFormState } from "src/useFormState";
