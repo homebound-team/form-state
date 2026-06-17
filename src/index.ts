fd2468bdf123562409a7ad7bbd6fc2bd59bb0cca
Allan Galdino <agaldino@homebound.com>
2026-06-17 11:54:26 +0100

    chore: export FormObjectState

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ export { type ListFieldState } from "src/fields/listField";
 export { createObjectState, type Fragment, type ObjectState, fragment } from "src/fields/objectField";
 export { type FieldState, type ValueAdapter } from "src/fields/valueField";
 export { required, type Rule } from "src/rules";
-export { useFormState } from "src/useFormState";
+export { type FormObjectState, useFormState } from "src/useFormState";
 export { useFormStates } from "src/useFormStates";
 export { type ObjectConfig } from "./config";
